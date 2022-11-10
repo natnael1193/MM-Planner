73595ffccec43f99736d1145807ecc0e54e24154
Natnael Solomon <natnaelsolomon770@gmail.com>
2022-11-10 09:28:27 +0300

    grid 6 changed to grid 12 on advert by station and programs

--- src/components/customComponents/advertComponent/AdvertByStationAndDaysDetailComponent.tsx
+++ src/components/customComponents/advertComponent/AdvertByStationAndDaysDetailComponent.tsx
@@ -21,7 +21,7 @@ const AdvertByStationAndDaysDetailComponent = ({
   };
   // console.log('showAds', showAds);
   return (
-    <Grid item lg={6} md={12} sm={12} xs={12}>
+    <Grid item lg={12} md={12} sm={12} xs={12}>
       <Card sx={{ p: 5 }}>
         <Grid container>
           <Grid item lg={2} md={2} sm={3} xs={3}>
